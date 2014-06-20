089a65966a436086c8da47f66acc8f204eff1890
justin <Justin.reiners@gmail.com>
2014-06-19 21:52:32 -0500

    python the hard way day 1

---
+++ exercise1/exercise1.py
@@ -0,0 +1,8 @@
+__author__ = 'justin'
+print "Hello World"
+print "Hello Again"
+print "I like typing this"
+print "This is fun."
+print "Yay! Printing"
+print "I would much rather you 'not'"
+print 'I "said" do not touch this'
---
+++ exercise1/exercise3.py
@@ -0,0 +1,14 @@
+__author__ = 'justin'
+# I will print something
+print " I will not count my chickens:"
+# basic math with python
+print "Hens", 25 + 30 / 6
+print "Roosters, 100", 100 - 25 * 3 % 4
+#harder math string
+print "I will not count my eggs:"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#find truth in a statement
+print "Is it true that 3 + 2 < 5 + 7?"
+print 3 + 2 < 5 - 7
+#yadda yadda
+print "What is 3 + 2?", 3 + 2
